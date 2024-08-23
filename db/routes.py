6074a5dfb304f174a9d70f947c8a5157a6b7e40e
jacobbailey8 <jbaileydsi87@gmail.com>
2024-08-23 03:50:42 -0400

    Added route to get movie by title

--- db/routes.py
+++ db/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from typing import List
 from database import get_db
 from models import Movie
@@ -17,9 +18,20 @@ def read_movies(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
 # Get a specific movie by ID
 
 
-@router.get("/movies/{movie_id}", response_model=MovieRead)
+@router.get("/movies/by-id/{movie_id}", response_model=MovieRead)
 def read_movie(movie_id: int, db: Session = Depends(get_db)):
     movie = db.query(Movie).filter(Movie.show_id == movie_id).first()
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return movie
+
+# Get a movie by title
+
+
+@router.get('/movies/by-title/{title}', response_model=MovieRead)
+def read_movie_by_title(title: str, db: Session = Depends(get_db)):
+    movie = db.query(Movie).filter(func.upper(
+        Movie.title) == title.upper()).first()
     if movie is None:
         raise HTTPException(status_code=404, detail="Movie not found")
     return movie
