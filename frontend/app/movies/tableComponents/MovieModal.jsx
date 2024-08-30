2ce910228b52a17d80e409c686915edecdb163a6
jacobbailey8 <jbaileydsi87@gmail.com>
2024-08-30 13:50:36 -0400

    poster comes up for movies

--- frontend/app/movies/tableComponents/MovieModal.jsx
+++ frontend/app/movies/tableComponents/MovieModal.jsx
@@ -1,15 +1,37 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 export default function Modal({ movie, closeModal }) {
 
+    const [img_url, setImgUrl] = React.useState('');
+
     useEffect(() => {
         async function fetchData() {
             if (movie) {
+                const API_KEY_TMDB = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+                const ACCESS_TOKEN = process.env.NEXT_PUBLIC_ACCESS_TOKEN_TMDB;
 
-                console.log(movie.title)
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: `Bearer ${ACCESS_TOKEN}`
+                    }
+                };
+
+
+
                 try {
+                    const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movie.title}`, options);
+                    const data = await res.json();
+
+                    // find image url, user rating if exists, 
+                    const firstResult = data.results[0];
+                    const img_url = 'https://image.tmdb.org/t/p/w500' + firstResult.poster_path;
+                    setImgUrl(img_url);
+
+
 
                 } catch (error) {
                     console.error(error)
@@ -37,8 +59,9 @@ export default function Modal({ movie, closeModal }) {
                 className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full"
                 onClick={(e) => e.stopPropagation()} // Prevent modal click from triggering overlay click
             >
+                <img src={img_url} alt={movie.title} className="w-full h-auto mb-4 rounded" />
                 <h2 className="text-lg font-bold mb-4">Modal Title</h2>
-                <p>This is the modal content.</p>
+
                 <button
                     className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
                     onClick={closeModal}
